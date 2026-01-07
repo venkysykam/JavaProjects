83e66be4cd4071dd306372d862cb90d8327bb1ad
SYKAM VENKATESH <177046912+venkysykam@users.noreply.github.com>
2026-01-07 14:06:20 +0530

    Add files via upload

---
+++ Bitwise Operations/BitWiseSettingTheithBit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class BitWiseSettingTheithBit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int i = sc.nextInt();
+        int bit = a | (1 << i);
+        System.out.println(bit);
+        sc.close();
+    }
+}
